46959fb9f1fd6348070ee99d26985e5c69a4a959
Grace Boehm <graceboehm@Graces-MacBook-Pro.local>
2018-02-14 13:38:08 -0500

    My group's Lab 4 COP3502

---
+++ IdeaProjects/Lab4/src/Main.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String currentNum;
+        int position = 0;
+        long answer = 0;
+        System.out.print("Enter a hexadecimal number: ");
+        currentNum = scanner.next();
+
+        if (currentNum.charAt(position) == '0' && currentNum.charAt(position + 1) == 'x'){
+            position = 2;
+        }
+
+        while (position < currentNum.length()){
+
+            int realNum;
+
+            switch (currentNum.charAt(position)){
+                case ('A'):
+                    realNum = 10;
+                    break;
+                case ('a'):
+                    realNum = 10;
+                    break;
+                case ('B'):
+                    realNum = 11;
+                    break;
+                case ('b'):
+                    realNum = 11;
+                    break;
+                case ('C'):
+                    realNum = 12;
+                    break;
+                case ('c'):
+                    realNum = 12;
+                    break;
+                case ('D'):
+                    realNum = 13;
+                    break;
+                case ('d'):
+                    realNum = 13;
+                    break;
+                case ('E'):
+                    realNum = 14;
+                    break;
+                case ('e'):
+                    realNum = 14;
+                    break;
+                case ('F'):
+                    realNum = 15;
+                    break;
+                case ('f'):
+                    realNum = 15;
+                    break;
+                default:
+                    realNum = currentNum.charAt(position) - 48;
+                }
+                long exponent = (long) Math.pow(16, currentNum.length() - position - 1);
+                long newNum = realNum * exponent;
+                answer += newNum;
+                position ++;
+
+
+        }
+
+        System.out.println("Your number is " + answer + " in decimal");
+    }
+}
